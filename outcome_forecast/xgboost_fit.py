b156fb5d7f9659c53b601f96ed30ab50adb83d55
rhys newbury <newbury777@gmail.com>
2023-12-07 16:44:28 +1100

    xgboost fixes

--- outcome_forecast/xgboost_fit.py
+++ outcome_forecast/xgboost_fit.py
@@ -9,8 +9,9 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 import xgboost
 import timeit
+import torch
 
-time = TimeRange(0, 6, 0.1)  # Minutes,
+time = TimeRange(0, 15, 0.3)  # Minutes,
 
 dataset = SC2Replay(
     Path(os.environ["DATAPATH"]),
@@ -31,27 +32,40 @@ for time_step, current_time in enumerate(time.arange()):
         os.remove(wins)
 
 t2 = timeit.default_timer()
+min_game_time = 5
 # process and save as numpy files
 for idx, sample in enumerate(dataloader):
     print(idx)
     print(f"Took {timeit.default_timer() - t2}s")
     t2 = timeit.default_timer()
 
+    difference_array = np.absolute(time.arange() - min_game_time)
+
+    # find the index of minimum element from the array
+    five_minute_index = difference_array.argmin()
+
+    mask = sample["valid"].sum(axis=1) > five_minute_index
+
     for time_step, current_time in enumerate(time.arange()):
         file = f"./{time_step}_x.npy"
         wins = f"./{time_step}_y.npy"
 
+        mask_ = torch.logical_and(sample["valid"][:, time_step], mask)
+        x = sample["scalar_features"][:, time_step, :][mask_, :]
+        y = sample["win"][mask_]
+
+        if mask_.sum() == 0:
+            break
+
         if Path(file).exists():
             data = np.load(file)
-            data = np.concatenate((data, sample["scalar_features"][:, time_step, :]))
+            data = np.concatenate((data, x))
 
             wins_array = np.load(wins)
-            wins_array = np.concatenate((wins_array, sample["win"]))
+            wins_array = np.concatenate((wins_array, y))
             np.save(str(file), data)
             np.save(str(wins), wins_array)
         else:
-            x = sample["scalar_features"][:, time_step, :]
-            y = sample["win"]
             np.save(str(file), x)
             np.save(str(wins), y)
 
@@ -62,8 +76,13 @@ for time_step, current_time in enumerate(time.arange()):
     print(f"Running current_time: {current_time}")
     file = f"./{time_step}_x.npy"
     wins = f"./{time_step}_y.npy"
+
+    if not (Path(file).exists() and Path(wins).exists()):
+        break
+
     x = np.load(file)
     y = np.load(wins)
+    print(f"for timestep = {time_step}, shape of data is {x.shape}")
     X = preprocessing.StandardScaler().fit(x).transform(x)
     random_state = 22
     cv = KFold(n_splits=5, shuffle=True, random_state=random_state)
