108580bfc63dccc46b2d5dae2332b0588f617fea
Bryce Ferenczi <frenzi@hotmail.com.au>
2024-01-14 22:34:47 +1100

    change lightspeed cli api and command formatting

--- conversion/bench_lightspeed.sh
+++ conversion/bench_lightspeed.sh
@@ -2,7 +2,9 @@
 start_time=$(date +%s)
 
 for i in $(seq 0 15); do
-    ./build/Release/lightspeed -p $((5670 + i)) >output_$i.txt 2>&1 &
+    ./lightspeed \
+        --replay /home/bryce/SC2/replays/4.9.2/00004e179daa5a5bafeef22c01bc84408f70052a7e056df5c63800aed85099e9.SC2Replay \
+        --game /home/bryce/SC2/game/4.9.2/Versions/Base74741/SC2_x64 -p $((5670 + i)) >output_$i.txt 2>&1 &
 done
 
 wait
