c74feabeb7f138bcdaaf57427858c95441099ca8
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2024-02-05 13:54:50 +1100

    add option to make lstm residual

--- outcome_forecast/src/model/lstm.py
+++ outcome_forecast/src/model/lstm.py
@@ -7,7 +7,13 @@ from konductor.models import MODEL_REGISTRY
 class LSTMDecoderV1(nn.Module):
     is_logit_output = True
 
-    def __init__(self, in_ch: int, hidden_size: int = 32, num_layers: int = 2):
+    def __init__(
+        self,
+        in_ch: int,
+        hidden_size: int = 32,
+        num_layers: int = 2,
+        residule_add: bool = False,
+    ):
         super().__init__()
         self.latent = nn.LSTM(
             input_size=in_ch,
@@ -19,14 +25,20 @@ class LSTMDecoderV1(nn.Module):
         self.c0 = nn.Parameter(torch.empty(num_layers, 1, hidden_size))
         torch.nn.init.xavier_normal_(self.h0)
         torch.nn.init.xavier_normal_(self.c0)
+
+        self.residule_add = residule_add
+        if residule_add:
+            self.residule_squeeze = nn.Linear(in_ch, hidden_size)
+
         self.decode = nn.Linear(hidden_size, 1)
 
     def forward(self, inputs: Tensor):
-        """Input is a [B, T, C] tensor, returns same shape"""
-        batch_sz, n_steps = inputs.shape[:2]
+        """Input is a [B, T, C] tensor, returns [B, T]"""
+        batch_sz = inputs.shape[0]
         h, c = self.h0.repeat(1, batch_sz, 1), self.c0.repeat(1, batch_sz, 1)
 
         out, _ = self.latent(inputs, (h, c))
-        outputs: Tensor = self.decode(out.reshape(batch_sz * n_steps, -1))
-        outputs = outputs.reshape(batch_sz, n_steps)
-        return outputs
+        if self.residule_add:
+            out = out + self.residule_squeeze(inputs)
+        outputs = self.decode(out)
+        return outputs.squeeze(-1)
