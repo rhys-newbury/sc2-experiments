8f70bf75116e52545e5f143fc029c2d6fe8dd8dd
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2024-02-05 22:48:52 +1100

    use transformer encoder with mask for causal time features, decode tokens individually

--- outcome_forecast/src/model/transformer.py
+++ outcome_forecast/src/model/transformer.py
@@ -13,32 +13,29 @@ class TransformerDecoderV1(nn.Module):
         max_time: float,
         hidden_size: int = 32,
         num_layers: int = 2,
+        num_head: int = 4,
         num_time_freq: int = 8,
         max_time_freq: int = 8,
-        num_latents: int = 8,
     ):
         """Max time in minutes, will be converted internally to gameloops"""
         super().__init__()
         self.squeeze = nn.Linear(in_ch, hidden_size)
-        self.decode = nn.Linear(num_latents * hidden_size, 1)
+        self.decode = nn.Linear(hidden_size + num_time_freq * 2, 1)
 
         self.max_time = max_time / (22.4 * 60)
         self.num_freq = num_time_freq
         self.max_freq = max_time_freq
 
-        self.transformer = nn.Transformer(
-            d_model=hidden_size + self.num_freq * 2,
-            num_encoder_layers=num_layers,
-            num_decoder_layers=1,
-            dim_feedforward=hidden_size * 2,
-            batch_first=True,
+        enc_layer = nn.TransformerEncoderLayer(
+            hidden_size + self.num_freq * 2, nhead=num_head
+        )
+        self.temporal_encoder = nn.TransformerEncoder(
+            encoder_layer=enc_layer,
+            num_layers=num_layers,
         )
 
-        self.latents = nn.Parameter(
-            torch.empty(num_latents, hidden_size + self.num_freq * 2)
-        )
-        with torch.no_grad():
-            self.latents.normal_(0.0, 0.5).clamp_(-2.0, 2.0)
+        self.mask: Tensor
+        self.register_buffer("mask", torch.empty([1]), persistent=False)
 
     @torch.no_grad()
     def create_time_embeddings(self, timepoints: Tensor):
@@ -54,19 +51,19 @@ class TransformerDecoderV1(nn.Module):
         ]
         return torch.cat(encodings, dim=-1)
 
+    def get_mask(self, time_dim: int) -> Tensor:
+        if time_dim != self.mask.shape[0]:
+            self.mask = nn.Transformer.generate_square_subsequent_mask(
+                time_dim, device=self.mask.device
+            )
+        return self.mask
+
     def forward(self, inputs: Tensor, timesteps: Tensor):
         """Input is a [B, T, C] tensor, timesteps is [B, T] in gamestep units, returns [B, T]"""
         time_embed = self.create_time_embeddings(timesteps)
         cat_features = torch.cat([self.squeeze(inputs), time_embed], dim=-1)
-        latents = self.latents.unsqueeze(0).expand(inputs.shape[0], -1, -1)
-        mask = self.transformer.generate_square_subsequent_mask(
-            cat_features.shape[1], device=cat_features.device
+        temporal_enc: Tensor = self.temporal_encoder(
+            cat_features, mask=self.get_mask(cat_features.shape[1]), is_causal=True
         )
-        out: Tensor = self.transformer(
-            cat_features, latents, src_mask=mask, src_is_causal=True
-        )
-        decoded: Tensor = self.decode(
-            out.view(inputs.shape[0], -1, self.latents.nelement())
-        )
-
+        decoded: Tensor = self.decode(temporal_enc)
         return decoded.squeeze(-1)
