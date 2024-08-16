Optimize diff_frame_mask calculation. Update sc2-serializer sample api. Fix redefine "filter" builtin.

forecasting/src/data/torch_dataset.py
@@ -100,7 +100,7 @@ class SC2ReplayOutcome(SC2ReplayBase):
         """Process replay data currently in parser into dictionary of
         features and game outcome"""
         try:
-            test_sample: dict[str, Any] = self.parser.sample(0)
+            test_sample: dict[str, Any] = self.parser.sample_all(0)
         except (RuntimeError, IndexError) as err:
             raise RuntimeError(f"Parse failure for {self.parser.info}") from err
 
@@ -121,7 +121,7 @@ class SC2ReplayOutcome(SC2ReplayBase):
             if idx == -1:
                 sample = {k: np.zeros_like(test_sample[k]) for k in outputs_list}
             else:
-                sample = self.parser.sample(int(idx.item()))
+                sample = self.parser.sample_all(int(idx.item()))
             for k in outputs_list:
                 outputs_list[k].append(sample[k])
 

forecasting/src/stats.py
@@ -349,7 +349,9 @@ class MinimapSoftIoU(Statistic):
         target_minimaps = targets["minimaps"][:, :, self.target_ch]
         next_minimap = target_minimaps[:, self.sequence_len :]
         static_unit_mask = torch.sum(target_minimaps, dim=1) != target_minimaps.shape[1]
-        diff_frame_mask = target_minimaps[:, [self.sequence_len - 1]] != next_minimap
+        diff_frame_mask = (
+            target_minimaps[:, self.sequence_len - 1, None] != next_minimap
+        )
 
         if self.should_sigmoid:
             predictions = torch.sigmoid(predictions)

forecasting/train.py
@@ -40,11 +40,11 @@ class Trainer(PyTorchTrainer):
         elif torch.cuda.is_available():
             stream = torch.cuda.Stream()
 
-            def filter(x: Tensor | list):
+            def filter_(x: Tensor | list):
                 return x.cuda(non_blocking=True) if isinstance(x, Tensor) else x
 
             with torch.cuda.stream(stream):
-                data = {k: filter(d) for k, d in data.items()}
+                data = {k: filter_(d) for k, d in data.items()}
 
             stream.synchronize()
 